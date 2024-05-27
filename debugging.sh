19fbbd79f554b27e6f6360d9eae50b911d5419b2
Abdilahi Kiula <kiulaabdilahi@gmail.com>
2024-05-27 14:22:51 +0200

    updated debugging.sh

--- debugging.sh
+++ debugging.sh
@@ -5,8 +5,7 @@ trap 'echo "The command [ ${BASH_COMMAND} ] on line number (${LINENO})"' DEBUG
 echo start
 
 for i in {1..4} asd {6..10}; do
-   (
-      [[ "${i}" == "asd" ]] && ${i}
+    [[ "${i}" == "asd" ]] && ${i}
    ) 1>/dev/null 2>&1 &
 done
 wait
